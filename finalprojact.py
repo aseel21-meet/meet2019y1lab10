01f54e7c3cf09b53850e0108f679ca41a7d06d89
aseel21-meet <aseel21@meet.mit.edu>
2019-08-04 11:01:17 -0400

    Stopping here

--- finalprojact.py
+++ finalprojact.py
@@ -1,6 +1,7 @@
 import turtle
-import random
-import winsound
+import time
+#import random
+#import winsound
 
 turtle.tracer(1.0)#MOVE SMOOTHLY
 
@@ -14,39 +15,82 @@ JELLY_FISH=[]
 SONGS_LIST=[]
 FUN_FACTS=[]
 
+SQUARE_SIZE=30
+TIME_STEP=100
+
 
 #create the game ored-design
 
-title=''
+title='trash crash'
 
 
 
-
+#the title amd instructions
 
 instructions='1.avoid the trash\n 2.eat the jellyfish\n 3.have fun'
 turtle.penup()
 turtle.goto(0,200)
-turtle.write(title,move=False,align='center',font=('arial',18,'normal')
+turtle.write(title,move=False,align='center',font=('arial',18,'normal'))
+turtle.hideturtle()
+instructor=turtle.Turtle()
+instructor.penup()
+instructor.goto(0,0)
+instructor.write(instructions,move=False,align='center',font=('arial',18,'normal'))
+instructor.hideturtle()
+time.sleep(5)
+instructor.clear()
+turtle.hideturtle()
+
+
+        
 
 
 
 seaturtle=turtle.Turtle()
+direction='none'
+def up ():
+    direction='up'
+    print('you pressed the up button')
+    seaturtle.register_shape('t4.gif')
+def right ():
+    direction='right'
+    print('you pressed the right button')
+    seaturtle.register_shape('t2.gif')
 
-def up ()
+def left():
+    direction='left'
+    print('oyu pressed the left button')
+    seaturtle.register_shape('t1.gif')
+def down ():
+    direction='down'
+    print('you pressed the down button')
+    turtle.register_shape('t5.gif')
 
-
-def right ()
-
-def left()
-
-def down ()
+turtle.onkeypress(up,'Up')
+turtle.onkeypress(down,'Down')
+turtle.onkeypress(left,'Left')
+turtle.onkeypress(right,'Right')
+turtle.listen()
 
 #make new turtle
 #level one
+seaturtle.penup()
+def move_seaturtle():
+    my_pos=seaturtle.pos()
+    x_pos=[0]
+    y_pos=[1]
+    if direction==up:
+        seaturtle.goto(x_pos,y_pos+SQUARE_SIZE)
+        print('moved up')
+    if directon==down:
+        seaturtle.goto(x_pos,y_pos-SQUARE_SIZE)
+        print('you moved down')
+        seaturtle.ontimer(move_seaturtle,TIME_STEP)
+        
+move_seaturtle()
 
 
 
-
-mainloop
+turtle.mainloop()
 
 
