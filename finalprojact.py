0e719d60f098ee9f72d33f7dd2bcb1c3bb698cb2
aseel21-meet <aseel21@meet.mit.edu>
2019-08-04 06:46:52 -0400

    Stopping here

---
+++ finalprojact.py
@@ -0,0 +1,52 @@
+import turtle
+import random
+import winsound
+
+turtle.tracer(1.0)#MOVE SMOOTHLY
+
+x_size=1000
+y_size=1000
+turtle.setup(x_size,y_size) #WINDOW SIZE
+
+TURTLE_LIST=['t1.gif','t2.gif','t3.gif','t4.gif','t5.gif']
+PLASTIC_LIST=['p1.gif','p2.gif','p3.gif','p4.gif','p5.gif']
+JELLY_FISH=[]
+SONGS_LIST=[]
+FUN_FACTS=[]
+
+
+#create the game ored-design
+
+title=''
+
+
+
+
+
+instructions='1.avoid the trash\n 2.eat the jellyfish\n 3.have fun'
+turtle.penup()
+turtle.goto(0,200)
+turtle.write(title,move=False,align='center',font=('arial',18,'normal')
+
+
+
+seaturtle=turtle.Turtle()
+
+def up ()
+
+
+def right ()
+
+def left()
+
+def down ()
+
+#make new turtle
+#level one
+
+
+
+
+mainloop
+
+
